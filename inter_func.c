64cc423bfdc64a2c6ef8af6d7d81270be3afe803
Daniel <danielems3@outlook.com>
2017-01-27 17:21:44 -0500

    make a .c for my function

--- inter_func.c
+++ inter_func.c
@@ -1,64 +1,26 @@
-#include "intersect.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sysexits.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-#include <strings.h>
-#include <stdbool.h>
+#include "inter_func.h"
 
 
 
-
-
-
-/*******************************************************************************
-void print_node(wordnode *root, int argv) 
-
-->The wordnode *root, points to the root node of the tree to be printed. 
-->The int argv, is the the current argv[] position in the array. 
-
-The function print_node recursively moves left, until it hits null, and then it 
-will print the char *word of the node as long as the root->count matches argv.
-	
-	**This is used to ensure that words are found in all the files that 
-	have been passed through the command line. 
-
-CITE: The function print_node was modified from the BSTnode exercise handed out 
-during Datastructures and Algorithims I.
-*******************************************************************************/
-void print_node(wordnode *root)
+void print_node(wordnode *root, int argv)
 {
 
 	if(root->left)
 	{
-		print_node(root->left);
+		print_node(root->left, argv);
 	}
-	printf("%s\n", root->word);
+	if(root->count == argv)
+	{
+		printf("%s\n", root->word);
+	}
 	if (root -> right)
 	{
-		print_node(root->right);
+		print_node(root->right, argv);
 	}
 	
 }
 
-/*******************************************************************************
-void destroy_wordtree(wordnode *root)
 
-->wordnode *root points to the root of the tree. 
-
-the function destroy_wordtree will move left looking for a null node, and then
-will recursvely free word, and then root. 
-
-CITE: The function destroy_wordtree was modified from the BSTnode exercise 
-handed out during Datastructures and Algorithims I.
-*******************************************************************************/
 void destroy_wordtree(wordnode *root){
      if (root == NULL) // or if (!root)
           return;
@@ -69,55 +31,42 @@ void destroy_wordtree(wordnode *root){
      free(root);
 } 
 
-/*******************************************************************************
-bool word_check(char *string, wordnode *root, int argv)
 
--> char *string is used as the check case for root->word.
--> wordnode *root is a pointer the beginning of the tree. 
--> int argv is the current.
- 
-The function word_check is used to see whether or not char *string is already in
-the tree. The int argv is used to be sure that the word did not enter the tree
-from the same file. This would be a false count step. 
-
-Bool is returned so I may
-check whether or not a word exists within a tree before the word inserts. 
-*******************************************************************************/
-bool word_check(char *string, wordnode *root)
+void word_check(char *string, wordnode *root, int argv)
 {
-	bool match = false;
+	//bool match = false;
 	if(root == NULL)
 	{
-		return false;
+		return;
 	}
 	
-	if(strcasecmp(string, root->word) == 0)
+	while(root != NULL && strcasecmp(string, root->word) != 0)
 	{
-		return true;
+		if(strcasecmp(string, root->word) < 0)
+		{
+			root = root->left;
+		}
+		else if(strcasecmp(string, root->word) > 0)
+		{
+			root = root->right;
+		}
+	}
+	if(root != NULL && strcasecmp(string, root->word) == 0)
+	{
+		if(root->count < argv)
+		{
+			root->count++;
+		}
+		return;
+	}
+	else 
+	{	
+		return;
 	}
 	
-	if(word_check(string, root->left))
-	{
-		return true;
-	}
-	match = word_check(string, root->right);
-	return match;
 }
 
-/*******************************************************************************
-wordnode *Insert(wordnode *root, char *string)
 
-->wordnote *root points to the root of the tree. 
-->char *string is the word that i am inserting.
-
-The function Insert will strcasecmp the string with root->words, and will then
-either insert the node to the left (if smaller), or to the right (if greater).
-The word is not inserted if the strcasecmp returns 0. The function then returns
-the pointer.
-
-CITE: The function Insert was modified from the BSTnode exercise given during
-Datastructres and Algorithims I.
-*******************************************************************************/
 wordnode *Insert(wordnode *root, char *string)
 {
 	if(root==NULL) 
@@ -132,8 +81,9 @@ wordnode *Insert(wordnode *root, char *string)
 		else
 		{
 			
-			root->word = calloc(1,256);
+			root->word = calloc(1,MAX_WORD_SIZE);
 			strncpy(root->word, string, strlen(string));
+			root->count = 1;
 			root->left = root->right = NULL;
 		}
 	}
@@ -146,6 +96,10 @@ wordnode *Insert(wordnode *root, char *string)
 		else if(strcasecmp(string, root->word) > 0)
 		{
 			root->right = Insert(root->right, string); 
+		}
+		else
+		{
+			return root;
 		}
 		
 	}
