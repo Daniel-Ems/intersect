e3ba21af00057289972935204355ccb8c8529dec
Daniel <danielems3@outlook.com>
2017-01-27 22:51:34 -0500

    Update comments and variable names

--- inter_func.c
+++ inter_func.c
@@ -2,106 +2,136 @@
 
 
 
-void print_node(wordnode *root, int argv)
+void
+print_tree (wordnode *tree, int argv)
 {
+  //The function will move to the bottom left. 
+  if (tree->left)
+    {
+      print_tree (tree->left, argv);
+    }
+  //The function will check to make sure the count is == to int argv. 
+  //int argv is the number of files passed to the program. If the count
+  //matches, it will print the tree->word. 
+  if (tree->count == argv)
+    {
+      printf ("%s\n", tree->word);
+    }
+  //The program will then move to the right recursively, and repeat the 
+  //process. 
+  if (tree->right)
+    {
+      print_tree (tree->right, argv);
+    }
 
-	if(root->left)
-	{
-		print_node(root->left, argv);
-	}
-	if(root->count == argv)
-	{
-		printf("%s\n", root->word);
-	}
-	if (root -> right)
-	{
-		print_node(root->right, argv);
-	}
-	
 }
 
 
-void destroy_wordtree(wordnode *root){
-     if (root == NULL) // or if (!root)
-          return;
-       
-     destroy_wordtree(root->left);  
-     destroy_wordtree(root->right);
-	 free(root->word);
-     free(root);
-} 
-
-
-void word_check(char *string, wordnode *root, int argv)
+void
+destroy_wordtree (wordnode * tree)
 {
-	//bool match = false;
-	if(root == NULL)
-	{
-		return;
-	}
-	
-	while(root != NULL && strcasecmp(string, root->word) != 0)
-	{
-		if(strcasecmp(string, root->word) < 0)
-		{
-			root = root->left;
-		}
-		else if(strcasecmp(string, root->word) > 0)
-		{
-			root = root->right;
-		}
-	}
-	if(root != NULL && strcasecmp(string, root->word) == 0)
-	{
-		if(root->count < argv)
-		{
-			root->count++;
-		}
-		return;
-	}
-	else 
-	{	
-		return;
-	}
-	
+  if (tree == NULL)
+    {
+      return;
+    }
+  //The function will recursively move to the bottom until it hits null.
+  //It will then free the tree->word and then the node.  
+  destroy_wordtree (tree->left);
+  destroy_wordtree (tree->right);
+  free (tree->word);
+  free (tree);
 }
 
 
-wordnode *Insert(wordnode *root, char *string)
+void
+word_check (char *string, wordnode * tree, int argv)
 {
-	if(root==NULL) 
-	{
-		root = (wordnode *) malloc(sizeof(*root));
 
-		if(root == NULL)
-		{
-			printf("Memory Error");
-			return NULL; 
-		}
-		else
-		{
-			
-			root->word = calloc(1,MAX_WORD_SIZE);
-			strncpy(root->word, string, strlen(string));
-			root->count = 1;
-			root->left = root->right = NULL;
-		}
-	}
-	else
+  if (tree == NULL)
+    {
+      return;
+    }
+
+  //strcasecmp returns -1,0,1 respectively according to the comparison between
+  //string and tree->word.
+  while (tree != NULL && strcasecmp (string, tree->word) != 0)
+    {
+      //If the result is less than the 0, the program needs to check left. 
+      if (strcasecmp (string, tree->word) < 0)
 	{
-		if(strcasecmp(string, root->word) < 0) 
-		{ 
-			root->left = Insert(root->left, string); 
-		}
-		else if(strcasecmp(string, root->word) > 0)
-		{
-			root->right = Insert(root->right, string); 
-		}
-		else
-		{
-			return root;
-		}
-		
+	  tree = tree->left;
 	}
-	return root; 
+      //If the reuslt is greater than 0, then the program needs to check right. 
+      else if (strcasecmp (string, tree->word) > 0)
+	{
+	  tree = tree->right;
+	}
+    }
+  //Once the loop breaks, it has either found a match, or has found null, and
+  //there for, no match. 
+  if (tree != NULL && strcasecmp (string, tree->word) == 0)
+    {
+      if (tree->count < argv)
+	{
+	  tree->count++;
+	}
+      return;
+    }
+  else
+    {
+      return;
+    }
+
+}
+
+
+wordnode *
+Insert (wordnode * tree, char *string)
+{
+  //result is the retrun value of strcasecmp. This prevents multiple calls to 
+  //strcasecmp.
+  int result;
+  //If tree == NULL, a new node is created.
+  if (tree == NULL)
+    {
+      //Mallocs for the size of the node.
+      tree = (wordnode *) malloc (sizeof (*tree));
+      if (tree == NULL)
+	{
+	  printf ("Memory Error");
+	  return NULL;
+	}
+      else
+	{
+	  //callocs for the size of the string. The string is then strncpy'd
+	  //into tree->word.
+	  tree->word = calloc (1, MAX_WORD_SIZE);
+	  strncpy (tree->word, string, strlen (string));
+	  //The count is initated to 1 because it is the first instance of 
+	  //the word. This can also be seen as the word was found in file 1.
+	  tree->count = 1;
+	  //The node's tree->left and tree->right are then initiated to NULL.
+	  tree->left = tree->right = NULL;
+	}
+    }
+  //If tree is not == NULL, then the results of strcasecmp is used to 
+  //appropriately place the word within the tree.
+  else
+    {
+      result = strcasecmp (string, tree->word);
+      if (result < 0)
+	{
+	  tree->left = Insert (tree->left, string);
+	}
+      else if (result > 0)
+	{
+	  tree->right = Insert (tree->right, string);
+	}
+      else
+	{
+	  return tree;
+	}
+
+    }
+  return tree;
 }
--- intersect.c
+++ intersect.c
@@ -1,116 +1,137 @@
 #include "inter_func.h"
 
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-	FILE *dictionaryFile;
+  FILE *dictionaryFile;
 
-    struct stat *buf = malloc(sizeof(*buf));
-	int i = 1;
-    off_t fileEnd;
+  //Struct stat is a built in structure that gives you multiple file 
+  //properties. It is being used here for the size of file property. 
+  struct stat *buf = malloc (sizeof (*buf));
+  int i = 1;
+  off_t fileEnd;
 
-	for(i = 1; i < argc; i++)
+  //The for loop iterates through all files passed through the command line.
+  //the for loop checks to make sure that none of the files are empty.
+  for (i = 1; i < argc; i++)
+    {
+      stat (argv[i], buf);
+      fileEnd = buf->st_size;
+      if (fileEnd == 0)
 	{
-    	stat(argv[i], buf);
-		fileEnd = buf->st_size;
-		if(fileEnd == 0)
-		{
-			printf("Empty Files not allowed\n");
-			return EX_USAGE;
-		}
+	  printf ("Empty Files not allowed\n");
+	  return EX_USAGE;
 	}
-
-
-    if (argc < 3)
-    {
-        printf("Please pass a file\n");
-        return EX_USAGE;
-    }
-    else
-    {
-       	dictionaryFile = fopen(argv[1], "r");
-
-        if (!dictionaryFile)
-        {
-            return EX_USAGE;
-        }
-
     }
 
+  //Thhis will check to be sure more than one file was passed to the program.
+  //If this is true, then the first file will be assigned to a file pointer.
+  if (argc < 3)
+    {
+      printf ("Please pass a file\n");
+      return EX_USAGE;
+    }
+  else
+    {
+      dictionaryFile = fopen (argv[1], "r");
 
-    char *tmpBuff = calloc(1, MAX_WORD_SIZE);
-	if(tmpBuff == NULL)
+      if (!dictionaryFile)
 	{
-		printf("calloc failed\n");
-		return EX_USAGE;
+	  return EX_USAGE;
 	}
 
- 	wordnode *root = NULL;
-	int count = 0;
-	char letter = 'a';
-	while(letter != EOF)
- 	{
-		letter = fgetc(dictionaryFile);
- 		tmpBuff[count] = letter;
-		if(isspace(letter))
-		{
-			tmpBuff[count] = '\0';
-			root = Insert(root, tmpBuff);
-			memset(tmpBuff, '\0', strlen(tmpBuff));
-			count = 0;
-		} 
-		else
-		{
-			count++;
-		}
-				
-	}
+    }
 
-	count = 0;
+  //This temporary buffer is used to bring in words. The pointer is calloced
+  //to the size of MAX_WORD_SIZE, an enumed value of 256, the maximum word
+  //size the program will see. 
+  char *tmpBuff = calloc (1, MAX_WORD_SIZE);
+  if (tmpBuff == NULL)
+    {
+      printf ("calloc failed\n");
+      return EX_USAGE;
+    }
 
-	int curFile = TEST_FILES;
-	FILE *intersectFiles;
-	while(curFile <= argc-1)
+  wordnode *root = NULL;
+  int count = 0;
+  char letter = 'a';
+  //This while loop will work with the first file and create a binary search
+  //tree that will act as a dictionary to check for matching words amongst
+  //the following files.
+  while (letter != EOF)
+    {
+      //Letter takes the first charachter of the first file
+      letter = fgetc (dictionaryFile);
+      //Letter is assigned to tmpBuff
+      tmpBuff[count] = letter;
+      //Letter is then checked whether or not it is a space. if it is then
+      //that position is set to a null byte to terminate the string, and then
+      //that string is passed to the insert function. 
+      if (isspace (letter))
 	{
-		intersectFiles = fopen(argv[curFile], "r");
-		if(!intersectFiles)
-		{
-			printf("Bad File\n");
-			return EX_USAGE;
-		}	
-		letter='a';
-		while(letter!=EOF)
-		{
-			letter = fgetc(intersectFiles);
-			tmpBuff[count] = letter;
-	
-			if(isspace(letter))
-			{
-				tmpBuff[count] ='\0';
-				word_check(tmpBuff, root, curFile);
-				memset(tmpBuff, '\0', strlen(tmpBuff));
-				count = 0;
-
-			}
-			else
-			{
-				count++;
-			}
-		}
-
-		fclose(intersectFiles);
-		memset(tmpBuff, '\0', strlen(tmpBuff));
-		curFile++;
+	  tmpBuff[count] = '\0';
+	  root = Insert (root, tmpBuff);
+	  //memset will set the buffer to null to allow for the next word. 
+	  memset (tmpBuff, '\0', strlen (tmpBuff));
+	  count = 0;
 	}
-						
-	print_node(root, argc -1);
+      else
+	{
+	  count++;
+	}
 
-	destroy_wordtree(root);
-	free(tmpBuff);
-	free(buf);
-	fclose(dictionaryFile);
-	//fclose(intersectFiles);
-	
+    }
+
+  count = 0;
+  //This for loop will cycle through the remaining files, and check the
+  //respectively against the dictionary tree already made.
+  int curFile = TEST_FILES;	//The current file position in argc. begins at 2.
+  FILE *intersectFiles;
+  while (curFile <= argc - 1)
+    {
+      intersectFiles = fopen (argv[curFile], "r");
+      if (!intersectFiles)
+	{
+	  printf ("Bad File\n");
+	  return EX_USAGE;
+	}
+      letter = 'a';
+      //This while loop will act similar to the one prior. 
+      while (letter != EOF)
+	{
+	  letter = fgetc (intersectFiles);
+	  tmpBuff[count] = letter;
+
+	  if (isspace (letter))
+	    {
+	      tmpBuff[count] = '\0';
+	      //Rather than sending the word to insert, it is sent to word_
+	      //checl where it will see if a word is in the dictionary. If so,
+	      //then the node counter is incrimented. 
+	      word_check (tmpBuff, root, curFile);
+	      memset (tmpBuff, '\0', strlen (tmpBuff));
+	      count = 0;
+
+	    }
+	  else
+	    {
+	      count++;
+	    }
+	}
+
+      fclose (intersectFiles);
+      memset (tmpBuff, '\0', strlen (tmpBuff));
+      curFile++;
+    }
+
+
+  print_tree (root, argc - 1);
+
+  destroy_wordtree (root);
+  free (tmpBuff);
+  free (buf);
+  fclose (dictionaryFile);
+
+
 }
-
- 
