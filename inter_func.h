17a328311a960a9b601c22a341143c4c888388a1
Daniel <danielems3@outlook.com>
2017-01-27 22:51:55 -0500

    Update comments and variable names

--- inter_func.h
+++ inter_func.h
@@ -16,8 +16,10 @@
 #include <stdbool.h>
 
 
-enum {TEST_FILES = 2};
-enum {MAX_WORD_SIZE = 256}; //This is the maximum size of a word.
+enum
+{ TEST_FILES = 2 };         //the 2nd file is the first to be tested.
+enum
+{ MAX_WORD_SIZE = 256 };	//This is the maximum size of a word.
 /*******************************************************************************
 typedef struct wordnode 
 
@@ -28,22 +30,24 @@ typedef struct wordnode
 CITE: The struct wordnode is a modification the the BSTnode we were given in
 Datastructures and Algortithims I.
 *******************************************************************************/
-typedef struct wordnode{
-     char *word;
-    struct wordnode *left;
-    struct wordnode *right;
-	int count;
+typedef struct wordnode
+{
+  char *word;
+  struct wordnode *left;
+  struct wordnode *right;
+  int count;
 
-}wordnode;
+} wordnode;
 
 /******************************************************************************
-void print_node(wordnode *root, int argv) 
+void print_node(wordnode *tree, int argv) 
 
-->The wordnode *root, points to the root node of the tree to be printed. 
+->The wordnode *tree, points to the node of the tree to be printed. 
 ->The int argv, is the the current argv[] position in the array. 
 
 The function print_node recursively moves left, until it hits null, and then it 
-will print the char *word of the node as long as the root->count matches argv.
+will print the tree->word of the node as long as the tree->count matches argv. 
+The function will then recursively move right and repeat the print process.
 	
 	**This is used to ensure that words are found in all the files that 
 	have been passed through the command line. 
@@ -51,51 +55,50 @@ will print the char *word of the node as long as the root->count matches argv.
 CITE: The function print_node was modified from the BSTnode exercise handed out 
 during Datastructures and Algorithims I.
 *******************************************************************************/
-void print_node(wordnode *root, int argv);
+void print_tree (wordnode *tree, int argv);
 
 /*******************************************************************************
-void destroy_wordtree(wordnode *root)
+void destroy_wordtree(wordnode *tree)
 
-->wordnode *root points to the root of the tree. 
+->wordnode *tree points to the node of the tree. 
 
-the function destroy_wordtree will move left looking for a null node, and then
-will recursvely free word, and then root. 
+the function destroy_wordtree will move until a null is found, and then
+will recursvely free tree->word, and then tree. 
 
 CITE: The function destroy_wordtree was modified from the BSTnode exercise 
 handed out during Datastructures and Algorithims I.
 *******************************************************************************/
-void destroy_wordtree(wordnode *root);
+void destroy_wordtree (wordnode *tree);
 
 /*******************************************************************************
-bool word_check(char *string, wordnode *root, int argv)
+void word_check(char *string, wordnode *tree, int argv)
 
--> char *string is used as the check case for root->word.
--> wordnode *root is a pointer the beginning of the tree. 
--> int argv is the current.
+-> char *string is used as the check case for tree->word.
+-> wordnode *tree is a pointer the beginning of the tree. 
+-> int argv is the current file being checked.
  
-The function word_check is used to see whether or not char *string is already in
-the tree. The int argv is used to be sure that the word did not enter the tree
-from the same file. This would be a false count step. 
-
-Bool is returned so I may
-check whether or not a word exists within a tree before the word inserts. 
+The function is passed *string which is used to check against all the words in
+the tree, wordnode *tree.  If the string is found, the node's counter is checked
+against int argv. If the counter is < int argv, then the counter is incrmented.
+ 
+CITE: Washington helped me nail down the logic for the word_check. 
 *******************************************************************************/
-void word_check(char *string, wordnode *root, int argv);
+void word_check (char *string, wordnode *tree, int argv);
 
 /*******************************************************************************
-wordnode *Insert(wordnode *root, char *string)
+wordnode *Insert(wordnode *tree, char *string)
 
-->wordnote *root points to the root of the tree. 
-->char *string is the word that i am inserting.
+->wordnote *tree points to the node of the tree. 
+->char *string is the word being inserted.
 
-The function Insert will strcasecmp the string with root->words, and will then
-either insert the node to the left (if smaller), or to the right (if greater).
-The word is not inserted if the strcasecmp returns 0. The function then returns
-the pointer.
+char *string is compared to tree->word using strcasecmp. if the string is < 0,
+the new node is created to the left of tree. If the string is > 0,
+the new node is created to the right of tree. If the string is == 0 no node is 
+created. If tree==NULL, a new node is created.
 
 CITE: The function Insert was modified from the BSTnode exercise given during
 Datastructres and Algorithims I.
 *******************************************************************************/
-wordnode *Insert(wordnode *root, char *string);
+wordnode *Insert (wordnode *tree, char *string);
 
 #endif
